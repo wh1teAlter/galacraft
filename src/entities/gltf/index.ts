f3131326f1309b42536c59bef96b866360d6c4cf
xiaobai.zzb <xiaobai.zzb@antgroup.com>
2023-04-28 19:34:28 +0800

    feat: 更新一个demo

--- src/entities/gltf/index.ts
+++ src/entities/gltf/index.ts
@@ -1,20 +1,20 @@
-import type { GLTFResource, Material, Vector3, WebGLEngine } from '@galacean/engine'
+import type { GLTFResource, Material, MeshTopology, Vector3, WebGLEngine } from '@galacean/engine'
 import { AssetType, Entity, MeshRenderer, ModelMesh } from '@galacean/engine'
 
 export async function createGLTF(engine: WebGLEngine) {
   const entity = new Entity(engine, 'gltf_entity')
-  const data = await engine.resourceManager.load<GLTFResource>({
+  let data = await engine.resourceManager.load<GLTFResource>({
     url: 'https://mdn.alipayobjects.com/afts/file/A*8MNtQIxVrJIAAAAAAAAAAAAADrd2AQ/Earth Voxels.gltf',
     type: AssetType.GLTF,
     params: {
       keepMeshData: true,
     },
   })
-  console.warn(data)
+  // console.warn(data)
   const position: Vector3[] = []
   const indices: number[] = []
   let count = 0
-  let topology
+  let topology: MeshTopology | undefined
   let offset = 0
 
   let material = <Material><unknown>null
@@ -42,7 +42,7 @@ export async function createGLTF(engine: WebGLEngine) {
   const modelMesh = new ModelMesh(engine)
   modelMesh.setPositions(position)
   modelMesh.setIndices(Uint32Array.from(indices))
-  modelMesh.addSubMesh(0, count, topology)
+  modelMesh.addSubMesh(0, 0, topology)
   modelMesh.uploadData(false)
   const meshRenderer = entity.addComponent(MeshRenderer)
   meshRenderer.mesh = modelMesh
@@ -50,5 +50,18 @@ export async function createGLTF(engine: WebGLEngine) {
 
   position.length = 0
   indices.length = 0
+
+  data.defaultSceneRoot.destroy()
+  data = null
+
+  let c = 0
+  setTimeout(() => {
+    setInterval(() => {
+      if (c < count)
+        c += 6
+      modelMesh.clearSubMesh()
+      modelMesh.addSubMesh(0, c, topology)
+    }, 10)
+  }, 2000)
   return { entity }
 }
